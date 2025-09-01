f6f3c506a95937f161636212210ccfb15dc9881e
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-09-01 22:22:43 +0530

    bubble sort

--- SortingAlgotithams/Java/BubbleSort/src/Main.java
+++ SortingAlgotithams/Java/BubbleSort/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] numbers = {4,39,19,0,56,83,2,76,10,94,21,12,94,76,46,24};
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = 0; j < numbers.length-1; j++){
+                if(numbers[j] > numbers[j+1]){
+                    int a = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = a;
+                }
+            }
+        }
+        for(int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i] + " ");
         }
     }
 }
--- SortingAlgotithams/Python/BubbleSort/main.py
+++ SortingAlgotithams/Python/BubbleSort/main.py
@@ -1,1 +1,6 @@
-print("hello")
+numbers = [4,39,19,0,56,83,2,76,10,94,21,12,94,76,46,24]
+for i in range(len(numbers)):
+    for j in range(len(numbers)-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+print(numbers)
